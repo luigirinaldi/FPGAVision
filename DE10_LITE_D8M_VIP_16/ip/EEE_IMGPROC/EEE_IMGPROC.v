c61044155aeeca5ed20598ad4e76430f3b7de114
luigirinaldi <luigirinaldi@users.noreply.github.com>
2023-06-07 21:23:32 +0100

    feat: update EEE imgproc to allow for setting of frame_averaging remotely

--- DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC.v
+++ DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC.v
@@ -1,33 +1,33 @@
 module EEE_IMGPROC(
-	// global clock & reset
-	clk,
-	reset_n,
-	
-	// mm slave
-	s_chipselect,
-	s_read,
-	s_write,
-	s_readdata,
-	s_writedata,
-	s_address,
+  // global clock & reset
+  clk,
+  reset_n,
+  
+  // mm slave
+  s_chipselect,
+  s_read,
+  s_write,
+  s_readdata,
+  s_writedata,
+  s_address,
 
-	// stream sink
-	sink_data,
-	sink_valid,
-	sink_ready,
-	sink_sop,
-	sink_eop,
-	
-	// streaming source
-	source_data,
-	source_valid,
-	source_ready,
-	source_sop,
-	source_eop,
-	
-	// conduit
-	mode
-	
+  // stream sink
+  sink_data,
+  sink_valid,
+  sink_ready,
+  sink_sop,
+  sink_eop,
+  
+  // streaming source
+  source_data,
+  source_valid,
+  source_ready,
+  source_sop,
+  source_eop,
+  
+  // conduit
+  mode
+  
 );
 
 
@@ -65,12 +65,14 @@ input                         mode;
 //
 parameter IMAGE_W = 11'd640;
 parameter IMAGE_H = 11'd480;
+
 parameter MESSAGE_BUF_MAX = 256;
 parameter MSG_INTERVAL = 6;
-parameter BB_COL = 24'h00ff00;
+
 parameter COL_DETECT_DEFAULT = 24'hFF0000; // detect red
 parameter COL_DETECT_THRESH_DEF = 17'hFFFF;
 parameter AVG_FRAMES = 10;
+
 parameter CROSSHAIR_COLOUR = 24'h00ff00; 
 parameter CROSSHAIR_SIZE = 10;
 
@@ -126,20 +128,20 @@ assign {red_out, green_out, blue_out} = (mode & ~sop & packet_video) ? new_image
 reg [10:0] x, y;
 reg packet_video;
 always@(posedge clk) begin
-	if (sop) begin
-		x <= 11'h0;
-		y <= 11'h0;
-		packet_video <= (blue[3:0] == 3'h0);
-	end
-	else if (in_valid) begin
-		if (x == IMAGE_W-1) begin
-			x <= 11'h0;
-			y <= y + 11'h1;
-		end
-		else begin
-			x <= x + 11'h1;
-		end
-	end
+  if (sop) begin
+    x <= 11'h0;
+    y <= 11'h0;
+    packet_video <= (blue[3:0] == 3'h0);
+  end
+  else if (in_valid) begin
+    if (x == IMAGE_W-1) begin
+      x <= 11'h0;
+      y <= y + 11'h1;
+    end
+    else begin
+      x <= x + 11'h1;
+    end
+  end
 end
 
 //Find first and last red pixels
@@ -149,66 +151,62 @@ reg [20:0] num_highs; // numbers of detected pixels
 
 //Process bounding box and centering at the end of the frame.
 reg [2:0] msg_state;
-reg [10:0] left, right, top, bottom;
 reg [7:0] frame_count;
 reg [10:0] centre_x, centre_y;
-reg [3:0] frame_averaging;
+reg [3:0] frame_averaging, max_num_frames;
 reg [31:0] prev_x, prev_y, prev_num;
 
 always@(posedge clk) begin
-	if (colour_detect & in_valid) begin	//Update bounds when the pixel is red
+  if (colour_detect & in_valid) begin	//Update bounds when the pixel is red
     num_highs <= num_highs + 1; // increment number of total detected pixels
     // increment sums
     sum_x <= sum_x + x;
     sum_y <= sum_y + y;
+  end
 
-		if (x < x_min) x_min <= x;
-		if (x > x_max) x_max <= x;
-		if (y < y_min) y_min <= y;
-		y_max <= y;	// because y is always increasing
-	end
-	if (sop & in_valid) begin	//Reset bounds on start of packet
-		x_min <= IMAGE_W-11'h1;
-		x_max <= 0;
-		y_min <= IMAGE_H-11'h1;
-		y_max <= 0;
-	end
+  // REMNANTS FROM BOUNDING BOX
+  // if (sop & in_valid) begin	//Reset bounds on start of packet
+  // 	x_min <= IMAGE_W-11'h1;
+  // 	x_max <= 0;
+  // 	y_min <= IMAGE_H-11'h1;
+  // 	y_max <= 0;
+  // end
 
-	if (eop & in_valid & packet_video) begin  //Ignore non-video packets
-		
-		//Latch edges for display overlay on next frame
-		left <= x_min;
-		right <= x_max;
-		top <= y_min;
-		bottom <= y_max;
-		
-    if (frame_averaging >= AVG_FRAMES) begin
+  if (eop & in_valid & packet_video) begin  //Ignore non-video packets
+    
+    //Latch edges for display overlay on next frame
+    // left <= x_min;
+    // right <= x_max;
+    // top <= y_min;
+    // bottom <= y_max;
+    
+    if (frame_averaging >= max_num_frames) begin
       centre_x <= sum_x / num_highs;
       centre_y <= sum_y / num_highs;
-			prev_x <= sum_x;
-			prev_y <= sum_y;
-			prev_num <= {11'b0,num_highs};
+      prev_x <= sum_x;
+      prev_y <= sum_y;
+      prev_num <= {11'b0,num_highs};
       sum_x <= 0;
       sum_y <= 0;
       num_highs <= 0;
       frame_averaging <= 0;
     end
     else frame_averaging <= frame_averaging + 1;
-		
-		//Start message writer FSM once every MSG_INTERVAL frames, if there is room in the FIFO
-		frame_count <= frame_count - 1;
-		
-		if (frame_count == 0 && msg_buf_size < MESSAGE_BUF_MAX - 3) begin
-			msg_state <= 3'b01;
-			frame_count <= MSG_INTERVAL-1;
-		end
-	end
-	
-	//Cycle through message writer states once started
-	if (msg_state != 3'b00) msg_state <= msg_state + 3'b01;
+    
+    //Start message writer FSM once every MSG_INTERVAL frames, if there is room in the FIFO
+    frame_count <= frame_count - 1;
+    
+    if (frame_count == 0 && msg_buf_size < MESSAGE_BUF_MAX - 3) begin
+      msg_state <= 3'b01;
+      frame_count <= MSG_INTERVAL-1;
+    end
+  end
+  
+  //Cycle through message writer states once started
+  if (msg_state != 3'b00) msg_state <= msg_state + 3'b01;
 
 end
-	
+  
 //Generate output messages for CPU
 reg [31:0] msg_buf_in; 
 wire [31:0] msg_buf_out;
@@ -220,77 +218,77 @@ wire msg_buf_empty;
 `define RED_BOX_MSG_ID "RBB"
 
 always@(*) begin	//Write words to FIFO as state machine advances
-	case(msg_state)
-		3'b000: begin
-			msg_buf_in = 32'b0;
-			msg_buf_wr = 1'b0;
-		end
-		3'b001: begin
-			msg_buf_in = `RED_BOX_MSG_ID;
-			msg_buf_wr = 1'b1;
-		end
-		3'b010: begin
-			msg_buf_in = prev_x;
-			msg_buf_wr = 1'b1;
-		end
-		3'b011: begin
-			msg_buf_in = prev_y;
-			msg_buf_wr = 1'b1;
-		end
-		3'b100: begin
-			msg_buf_in = prev_num;
-			msg_buf_wr = 1'b1;
-		end
-		3'b101: begin
-			msg_buf_in = centre_x;
-			msg_buf_wr = 1'b1;
-		end
-		3'b110: begin
-			msg_buf_in = centre_y;
-			msg_buf_wr = 1'b1;
-		end
-		default: begin
-			msg_buf_in = 32'b0;
-			msg_buf_wr = 1'b1;
-		end
-	endcase
+  case(msg_state)
+    3'b000: begin
+      msg_buf_in = 32'b0;
+      msg_buf_wr = 1'b0;
+    end
+    3'b001: begin
+      msg_buf_in = `RED_BOX_MSG_ID;
+      msg_buf_wr = 1'b1;
+    end
+    3'b010: begin
+      msg_buf_in = prev_x;
+      msg_buf_wr = 1'b1;
+    end
+    3'b011: begin
+      msg_buf_in = prev_y;
+      msg_buf_wr = 1'b1;
+    end
+    3'b100: begin
+      msg_buf_in = prev_num;
+      msg_buf_wr = 1'b1;
+    end
+    3'b101: begin
+      msg_buf_in = centre_x;
+      msg_buf_wr = 1'b1;
+    end
+    3'b110: begin
+      msg_buf_in = centre_y;
+      msg_buf_wr = 1'b1;
+    end
+    default: begin
+      msg_buf_in = 32'b0;
+      msg_buf_wr = 1'b1;
+    end
+  endcase
 end
 
 
 //Output message FIFO
 MSG_FIFO	MSG_FIFO_inst (
-	.clock (clk),
-	.data (msg_buf_in),
-	.rdreq (msg_buf_rd),
-	.sclr (~reset_n | msg_buf_flush),
-	.wrreq (msg_buf_wr),
-	.q (msg_buf_out),
-	.usedw (msg_buf_size),
-	.empty (msg_buf_empty)
-	);
+  .clock (clk),
+  .data (msg_buf_in),
+  .rdreq (msg_buf_rd),
+  .sclr (~reset_n | msg_buf_flush),
+  .wrreq (msg_buf_wr),
+  .q (msg_buf_out),
+  .usedw (msg_buf_size),
+  .empty (msg_buf_empty)
+  );
 
 
 //Streaming registers to buffer video signal
 STREAM_REG #(.DATA_WIDTH(26)) in_reg (
-	.clk(clk),
-	.rst_n(reset_n),
-	.ready_out(sink_ready),
-	.valid_out(in_valid),
-	.data_out({red,green,blue,sop,eop}),
-	.ready_in(out_ready),
-	.valid_in(sink_valid),
-	.data_in({sink_data,sink_sop,sink_eop})
+  .clk(clk),
+  .rst_n(reset_n),
+  .ready_out(sink_ready),
+  .valid_out(in_valid),
+  .data_out({red,green,blue,sop,eop}),
+  .ready_in(out_ready),
+  .valid_in(sink_valid),
+  .data_in({sink_data,sink_sop,sink_eop})
 );
 
 STREAM_REG #(.DATA_WIDTH(26)) out_reg (
-	.clk(clk),
-	.rst_n(reset_n),
-	.ready_out(out_ready),
-	.valid_out(source_valid),
-	.data_out({source_data,source_sop,source_eop}),
-	.ready_in(source_ready),
-	.valid_in(in_valid),
-	.data_in({red_out, green_out, blue_out, sop, eop})
+  .clk(clk),
+  .rst_n(reset_n),
+  .ready_out(out_ready),
+  .valid_out(source_valid),
+  .data_out({source_data,source_sop,source_eop}),
+  .ready_in(source_ready),
+  .valid_in(in_valid),
+  .data_in({red_out, green_out, blue_out, sop, eop})
 );
 
 
@@ -320,19 +318,25 @@ reg [23:0]  des_colour;
 
 always @ (posedge clk)
 begin
-	if (~reset_n)
-	begin
-		reg_status <= 8'b0;
+  if (~reset_n)
+  begin
+    reg_status <= 8'b0;
     des_colour <= COL_DETECT_DEFAULT;
-		col_detect_thresh <= COL_DETECT_THRESH_DEF;
-	end
-	else begin
-		if(s_chipselect & s_write) begin
-		   if      (s_address == `REG_STATUS)	reg_status <= s_writedata[7:0];
-		   if      ((s_address == `REG_BBCOL) && s_writedata[31])	col_detect_thresh <= s_writedata[17:0]; // update threshold
-       if      ((s_address == `REG_BBCOL) && !s_writedata[31])	des_colour <= s_writedata[23:0]; // update colour
-		end
-	end
+    col_detect_thresh <= COL_DETECT_THRESH_DEF;
+    max_num_frames <= AVG_FRAMES;
+  end
+  else begin
+    if(s_chipselect & s_write) begin
+      if      (s_address == `REG_STATUS)	reg_status <= s_writedata[7:0];
+      if      (s_address == `REG_BBCOL) begin
+        case(s_writedata[31:30])
+          2'b00:  col_detect_thresh <= s_writedata[17:0]; // update threshold
+          2'b01:  des_colour <= s_writedata[23:0]; // update colour
+          2'b10:  max_num_frames <= s_writedata[15:0];
+        endcase
+      end 
+    end
+  end
 end
 
 
@@ -347,23 +351,22 @@ reg read_d; //Store the read signal for correct updating of the message buffer
 always @ (posedge clk)
 begin
    if (~reset_n) begin
-	   s_readdata <= {32'b0};
-		read_d <= 1'b0;
-	end
-	
-	else if (s_chipselect & s_read) begin
-		if   (s_address == `REG_STATUS) s_readdata <= {16'b0,msg_buf_size,reg_status};
-		if   (s_address == `READ_MSG) s_readdata <= {msg_buf_out};
-		if   (s_address == `READ_ID) s_readdata <= 32'h1234EEE2;
-		// if   (s_address == `REG_BBCOL) s_readdata <= {8'h0, bb_col};
-	end
-	
-	read_d <= s_read;
+     s_readdata <= {32'b0};
+    read_d <= 1'b0;
+  end
+  
+  else if (s_chipselect & s_read) begin
+    if   (s_address == `REG_STATUS) s_readdata <= {16'b0,msg_buf_size,reg_status};
+    if   (s_address == `READ_MSG) s_readdata <= {msg_buf_out};
+    if   (s_address == `READ_ID) s_readdata <= 32'h1234EEE2;
+    if   (s_address == `REG_BBCOL) s_readdata <= 32'h0; // send something back otherwise it doens't like it
+  end
+  
+  read_d <= s_read;
 end
 
 //Fetch next word from message buffer after read from READ_MSG
 assign msg_buf_rd = s_chipselect & s_read & ~read_d & ~msg_buf_empty & (s_address == `READ_MSG);
-						
 
 
 endmodule
